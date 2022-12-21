525f57960c65c49078110e29645e739fd6b4dfbf
nessma97 <nessmaelsheikh97@gmail.com>
2022-12-21 18:36:25 +0200

    Deleted All Logs

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@
 const drodownBtn = document.querySelector("#dropdown-btn");
 const menu = document.querySelector("#menu");
 
-console.log(drodownBtn);
-console.log(menu);
 drodownBtn.addEventListener("click", () => {
-  console.log("clicked");
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
   } else {
